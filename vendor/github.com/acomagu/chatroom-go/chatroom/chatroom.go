255cd158e0c40da82faba654637ca50a70921b62
Yuki Ito <acomagu@gmail.com>
2017-02-15 14:23:28 +0900

    [Add] deps for heroku

---
+++ vendor/github.com/acomagu/chatroom-go/chatroom/chatroom.go
@@ -0,0 +1,78 @@
+package chatroom
+
+import ()
+
+type roomInternal struct {
+	in  chan interface{}
+	out chan interface{}
+}
+
+// A Chatroom has all functions and channels to be exported from this package.
+type Chatroom struct {
+	// Entry RoomEntry
+	topics []Topic
+	entry roomInternal
+}
+
+// A Room has functions to wait or send messages with user. This is passed to Topic function as argument.
+type Room interface {
+	// WaitMsg waits and returns the message inputed by Chatroom#Flush.
+	WaitMsg() interface{}
+
+	// WaitMsg waits and returns the string message inputed by Chatroom#Flush. This ignores the other type values.
+	WaitTextMsg() string
+
+	// Send inputs the value to be sent to user. It will be got by Chatroom#WaitSentMsg or Chatroom#WaitSentTextMsg.
+	Send(interface{})
+}
+
+// Flush inputs the value to be passed to Topic.
+func (cr Chatroom) Flush(v interface{}) {
+	cr.entry.in <- v
+}
+
+// WaitSentMsg waits and returns the value passed to all of Room#Send. Used to send messages to user (through chat service).
+func (cr Chatroom) WaitSentMsg() interface{} {
+	text := <-cr.entry.out
+	return text
+}
+
+// WaitSentTextMsg waits and returns the string value. It ignores the others.
+func (cr Chatroom) WaitSentTextMsg() string {
+	for {
+		if str, ok := cr.WaitSentMsg().(string); ok {
+			return str
+		}
+	}
+}
+
+func (room roomInternal) WaitMsg() interface{} {
+	return <-room.in
+}
+
+func (room roomInternal) WaitTextMsg() string {
+	for {
+		if str, ok := (<-room.in).(string); ok {
+			return str
+		}
+	}
+}
+
+func (room roomInternal) Send(v interface{}) {
+	room.out <- v
+}
+
+// New creates and initialize a Chatroom. This also starts a go-routine to pass messages to Topics.
+func New(topics []Topic) Chatroom {
+	_room := roomInternal{
+		in: make(chan interface{}),
+		out: make(chan interface{}),
+	}
+	chatroom := Chatroom{
+		// Entry: newRoomEntry(_room),
+		topics: topics,
+		entry: _room,
+	}
+	go chatroom.talk(_room)
+	return chatroom
+}
---
+++ vendor/github.com/acomagu/chatroom-go/chatroom/talk.go
@@ -0,0 +1,120 @@
+package chatroom
+
+import (
+	"fmt"
+)
+
+// topicChan includes chatroom channel, the channel pass the returned value from topic.
+type topicChan struct {
+	Chatroom roomInternal
+	Return   chan bool
+}
+
+// A DidTalk means whether the Topic talks with user. A Topic function must return this type value.
+type DidTalk bool
+
+// Topic type is the function express a bunch of flow in chattting. Pass slice of this to New(), and the function called them in order. If one of them returns true, the loop breaks.
+type Topic func(Room) DidTalk
+
+func (cr Chatroom) talk(chatroom roomInternal) {
+	topicChans := []topicChan{}
+	for _, topic := range cr.topics {
+		topicChan := loopTopic(topic, chatroom)
+		topicChans = append(topicChans, topicChan)
+	}
+	middleChatroom, clearPool, broadcastPool := poolMessages(chatroom)
+	changeDestTopicTo := distributeMessage(middleChatroom)
+	go controller(topicChans, changeDestTopicTo, broadcastPool, clearPool)
+}
+
+func controller(topicChans []topicChan, changeDestTopicTo chan roomInternal, broadcastPool chan bool, clearPool chan bool) {
+	for {
+		for i, topicChan := range topicChans {
+			changeDestTopicTo <- topicChan.Chatroom
+			if i > 0 { // for the start time.
+				broadcastPool <- true
+			}
+			didTalk := <-topicChan.Return
+			if didTalk {
+				clearPool <- true
+				break
+			}
+		}
+		clearPool <- true
+	}
+}
+
+// This pipe stores messages from user with flowing next Chatroom(middleChatroom). And this provides functions, clearPool and broadcastPool. This is used in controller().
+func poolMessages(chatroom roomInternal) (roomInternal, chan bool, chan bool) {
+	middleChatroom := roomInternal{
+		in:  make(chan interface{}),
+		out: chatroom.out,
+	}
+	clearPool := make(chan bool)
+	broadcastPool := make(chan bool)
+
+	go func(chatroom roomInternal, middleChatroom roomInternal, clearPool <-chan bool, broadcastPool <-chan bool) {
+		var pool []interface{}
+		for {
+			select {
+			case message := <-chatroom.in:
+				pool = append(pool, message)
+				middleChatroom.in <- message
+
+			case <-clearPool:
+				pool = pool[:0]
+
+			case <-broadcastPool:
+				for _, message := range pool {
+					middleChatroom.in <- message
+				}
+			}
+		}
+	}(chatroom, middleChatroom, clearPool, broadcastPool)
+
+	return middleChatroom, clearPool, broadcastPool
+}
+
+// distributeMessage pass message from chatroom to chatroom. The chatroom of destination will change as needed, changed by value of channel, changeDestTopicTo.
+func distributeMessage(middleChatroom roomInternal) chan roomInternal {
+	changeDestTopicTo := make(chan roomInternal)
+
+	go func(middleChatroom roomInternal, changeDestTopicTo <-chan roomInternal) {
+		var dest roomInternal
+		dest = <-changeDestTopicTo
+		for {
+			select {
+			case message := <-middleChatroom.in:
+				if dest == (roomInternal{}) {
+					fmt.Println("Error: the destination chatroom is not set.")
+					break
+				}
+				dest.in <- message
+
+			case _dest := <-changeDestTopicTo:
+				dest = _dest
+			}
+		}
+	}(middleChatroom, changeDestTopicTo)
+
+	return changeDestTopicTo
+}
+
+// loopTopic just loops topic.
+func loopTopic(topic Topic, chatroom roomInternal) topicChan {
+	tc := topicChan{
+		Chatroom: roomInternal{
+			in:  make(chan interface{}),
+			out: chatroom.out,
+		},
+		Return: make(chan bool),
+	}
+
+	go func(topic Topic, tc topicChan) {
+		for {
+			tc.Return <- bool(topic(tc.Chatroom))
+		}
+	}(topic, tc)
+
+	return tc
+}
