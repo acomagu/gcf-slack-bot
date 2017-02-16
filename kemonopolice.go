add4aeff057d492652a1e1947fbb3156e0f354c4
Yuki Ito <acomagu@gmail.com>
2017-02-16 11:47:25 +0900

    [Modify] Allow `...ね!` sentence in `kemono` channel

---
+++ kemonopolice.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"github.com/acomagu/chatroom-go/chatroom"
+)
+
+var laws = []*regexp.Regexp{
+	regexp.MustCompile(`すっ?ごーい`),
+	regexp.MustCompile(`たー?のしー`),
+	regexp.MustCompile(`わーい`),
+	regexp.MustCompile(`へーきへーき`),
+	regexp.MustCompile(`[^!！。、]+ね`),
+	regexp.MustCompile(`フレンズによって(得意|とくい)なこと(違|ちが)うから`),
+	regexp.MustCompile(`うわー`),
+	regexp.MustCompile(`なにこれなにこれ`),
+	regexp.MustCompile(`おもしろーい`),
+}
+
+func kemonoPolice(room chatroom.Room) chatroom.DidTalk {
+	r := waitReceived(room)
+	if isLegal(r.text) {
+		return false
+	}
+	_, _, err := api.DeleteMessage(r.channelID, r.timestamp)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return true
+}
+
+func isLegal(msg string) bool {
+	removes := regexp.MustCompile(`(\s|\t|\n|!|！)`)
+	msg = removes.ReplaceAllString(msg, "")
+
+	for _, law := range laws {
+		msg = law.ReplaceAllString(msg, "")
+	}
+	return msg == ""
+}
+
+func waitReceived(room chatroom.Room) received {
+	for {
+		msg := room.WaitMsg()
+		if r, ok := msg.(received); ok {
+			return r
+		}
+	}
+}
