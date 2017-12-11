8bebf01b46233bc6d59bb2ec58e8754f105357f4
Yuki Ito <acomagu@gmail.com>
2017-12-11 18:53:07 +0900

    [Add] nanhankanmu

---
+++ nanhankanmu/nanhankanmu.go
@@ -0,0 +1,23 @@
+package nanhankanmu
+
+import (
+	"github.com/acomagu/chatroom-go/chatroom"
+	"github.com/acomagu/gcf-slack-bot/topicutil"
+	"regexp"
+)
+
+var trigger = regexp.MustCompile(`3[^\w]*3[^\w]*4`)
+
+// Client keeps slack client to delete other's message.
+type Client struct {}
+
+// Talk is main Topic
+func (Client) Talk(room chatroom.Room) chatroom.DidTalk {
+	r := topicutil.WaitReceived(room)
+	if !trigger.MatchString(r.Text) {
+		return false
+	}
+
+	room.Send("なんでや! 阪神関係ないやろ!")
+	return true
+}
--- topics.go
+++ topics.go
@@ -5,10 +5,12 @@ import (
 	"github.com/acomagu/gcf-slack-bot/slackcr"
 	"github.com/acomagu/gcf-slack-bot/restaurants"
 	"github.com/acomagu/gcf-slack-bot/kmnreact"
+	"github.com/acomagu/gcf-slack-bot/nanhankanmu"
 )
 
 func topics(clients slackcr.SlackClients) []chatroom.Topic {
 	rests := restaurants.New(clients.Friends)
 	react := kmnreact.New(clients.God)
-	return []chatroom.Topic{rests.Talk, react.Talk}
+	nnkm := nanhankanmu.Client{}
+	return []chatroom.Topic{rests.Talk, react.Talk, nnkm.Talk}
 }
