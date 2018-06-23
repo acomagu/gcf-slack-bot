2069f0f8d1de7ffe54c2e7a64495fdd4c102ad14
Yuki Ito <acomagu@gmail.com>
2018-06-23 17:05:28 +0900

    Add twada

--- topics.go
+++ topics.go
@@ -6,11 +6,13 @@ import (
 	"github.com/acomagu/gcf-slack-bot/restaurants"
 	"github.com/acomagu/gcf-slack-bot/kmnreact"
 	"github.com/acomagu/gcf-slack-bot/nanhankanmu"
+	"github.com/acomagu/gcf-slack-bot/twada"
 )
 
 func topics(clients slackcr.SlackClients) []chatroom.Topic {
 	rests := restaurants.New(clients.Friends)
 	react := kmnreact.New(clients.God)
 	nnkm := nanhankanmu.Client{}
-	return []chatroom.Topic{rests.Talk, react.Talk, nnkm.Talk}
+	twada := twada.Client{}
+	return []chatroom.Topic{rests.Talk, react.Talk, nnkm.Talk, twada.Talk}
 }
